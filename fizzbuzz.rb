61b45d484e1aee92ed557773f8e7fcd4c7312597
Arielle N Ramirez <ariellenramirez@gmail.com>
2018-07-25 12:30:10 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,9 +3,9 @@ def fizzbuzz(int)
     return "Fizz"
   elsif int % 5 == 0
     return "Buzz"
-  elsif (int % 5 && int % 3) == 0
+  elsif int % 5 && int % 3 == 0
     return "FizzBuzz"
-  else (int % 5 || int % 3) !== 0 
+  else int % 5 || int % 3 !== 0 
     return "nil"
 end
 end
